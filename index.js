7df490febc06c7889740e5957d639e1b48efb54e
20leo02 <lrm2188@columbia.edu>
2024-04-11 19:45:36 -0400

    Change JS structure

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
+let secretWord = "";
+let utterance = new SpeechSynthesisUtterance();
 
-$(document).ready(function() {
-    let secretWord = "";
-    let utterance = new SpeechSynthesisUtterance();
+function handleKeyPress(event) {
+    utterance.text = event.key;
+    window.speechSynthesis.speak(utterance);
+}
 
+function getSecretWord() {
+    const jsonData = require('.Accessible-Wordle/root/server/words.json');
+    const randomIndex = Math.floor(Math.random() * jsonData.length);
+    secretWord = jsonData[randomIndex];
+    console.log(secretWord);
+}
+
+$(function(){
     getSecretWord();
     document.addEventListener('keypress', handleKeyPress);
-
-    function handleKeyPress(event) {
-        utterance.text = event.key;
-        window.speechSynthesis.speak(utterance);
-    }
-
-    function getSecretWord() {
-        const jsonData = require('.Accessible-Wordle/root/server/words.json');
-        const randomIndex = Math.floor(Math.random() * jsonData.length);
-        secretWord = jsonData[randomIndex];
-        console.log(secretWord);
-    }
 
     // function checkWord(word) {
     //     if (word === secretWord) {
