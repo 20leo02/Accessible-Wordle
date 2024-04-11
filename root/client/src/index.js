265f0841ce97854ba23e5675267e611e3922eda6
agubata <joshuaagubata@gmail.com>
2024-04-10 21:36:59 -0400

    another init

---
+++ root/client/src/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    let secretWord = "";
+    let utterance = new SpeechSynthesisUtterance();
+
+    getSecretWord();
+    document.addEventListener('keypress', handleKeyPress);
+
+    function handleKeyPress(event) {
+        utterance.text = event.key;
+        window.speechSynthesis.speak(utterance);
+    }
+
+    function getSecretWord() {
+        const jsonData = require('.Accessible-Wordle/root/server/words.json');
+        const randomIndex = Math.floor(Math.random() * jsonData.length);
+        secretWord = jsonData[randomIndex];
+        console.log(secretWord);
+    }
+
+    // function checkWord(word) {
+    //     if (word === secretWord) {
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
+
+})
